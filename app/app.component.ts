991a08cc672cd00f7b69a21c95c71db02a451137
Tim Sarvey <timsarvey@gmail.com>
2017-01-24 13:43:54 -0500

    Start work on 8 ball.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  			<div class='ball'>
+  				<input class='question-input' type='text' placeholder='Ask a yes or no question'><button class='question-submit'>Ask</button>
+  				<div class='inner-circle'>8</div>
+  			</div>
+  			`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {}
