dfcc2d0438ac82f7d0b0e1e3202e7e10b321f3a9
Tim Sarvey <timsarvey@gmail.com>
2017-02-24 13:17:26 -0500

    Add animations. Add more answers. Working version of eight ball!

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,7 +18,7 @@ require('../node_modules/velocity-animate/velocity.ui.min.js');
   			</question-input>
 			  <eight-ball></eight-ball>
         <answer message='{{answer}}'></answer>
-        <div class='center'>
+        <div class='center reset-container'>
           <div class='hidden' id='reset' (click)='reset()'>Ask Another Question</div>
         </div>
   			`
@@ -26,13 +26,23 @@ require('../node_modules/velocity-animate/velocity.ui.min.js');
 export class AppComponent  {
   constructor(private answersService: AnswersService){}
   answer:string;
+  rotations = [5,-5,20,-20,15,-15,10,-10];
 	askedQuestion(question: string){
     this.answer = this.answersService.getAnswer();
-    $('.question-container').velocity({translateY: "-2000px"}, {duration: 500});
+    $('.question-container').velocity('transition.bounceUpOut', {duration: 500});
+    $('.eight').delay(1000).velocity('transition.bounceRightIn', {duration: 500, display: 'table-cell'}).delay(500).velocity('callout.shake', {duration: 1000}).delay(500).velocity('transition.bounceLeftOut', {duration: 500});
+    $('.answer-container').delay(5000).velocity('transition.bounceRightIn', {duration: 500});
+    $('.triangle').delay(6500).velocity('transition.fadeIn', {duration: 2000})
+    setTimeout(()=> {
+      $('.triangle').velocity({rotateZ: this.rotations[Math.floor(Math.random() * this.rotations.length)]}, {queue: false, duration: 2000});
+    }, 6500);
+    $('#reset').delay(9000).velocity('transition.bounceRightIn', {display: 'inline-block'});
 	}
   reset(){
-    this.showAnswer = false;
-    this.showEight = false;
-    this.showInput = true;
+    $('#reset').velocity('transition.bounceLeftOut');
+    $('.answer-container').velocity('transition.bounceLeftOut');
+    $('.triangle').velocity('transition.fadeOut');
+    $('.question-container').velocity('transition.bounceDownIn', {duration: 500});
+
   }
 }
--- app/question.component.ts
+++ app/question.component.ts
@@ -11,7 +11,7 @@ let $ = require('../node_modules/jquery/dist/jquery.min.js')
 					(ngModelChange)='onChange($event)'>
 				<div 
 					class='question-submit'
-					(click)='submit(question); question = "";'>
+					(click)='submit(question); question = ""'>
 					{{buttonLabel}}
 				</div>
 			</div>
@@ -23,13 +23,18 @@ export class Question{
 	@Input() buttonLabel:string;
 	@Output() onSubmit = new EventEmitter<string>();
 	submit(question: string) {
-		console.log(question);
+		console.log('submit called');
+		this.showSubmit === false;
 		this.onSubmit.emit(question);
+		this.onChange('');
 	}
 	onChange(newValue:string){
+		console.log(typeof newValue);
+		console.log(newValue.length);
+		console.log(this.showSubmit);
 		if(newValue.length && this.showSubmit === false){
 			this.showSubmit = true;
-			$('.question-submit').velocity('fadeIn', { display: 'inline-block',duration: 1500});
+			$('.question-submit').velocity('fadeIn', { display: 'inline-block',duration: 500});
 		} else if(!newValue.length) {
 			this.showSubmit = false;
 			$('.question-submit').hide();
